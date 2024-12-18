7ae959b2d797e65b5d95dc1f7053c22a56985c80
Khalidur34 <khalidur34@gmail.com>
2024-12-18 14:19:27 +1100

    third exercise done

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let final = "";
+    for(let i = string.length - 1 ; i >= 0 ; i--) {
+        final += string.at(i);
+    }
+    return final;
 };
 
 // Do not edit below this line
