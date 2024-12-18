641ce1d33132bb3307bc6dd0a69c3debf505990c
Khalidur34 <khalidur34@gmail.com>
2024-12-18 14:11:21 +1100

    exercise 2 completed and all tests passes

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, number) {
+    if(number < 0) {
+        return "ERROR";
+    }
+    final = "";
+    for(let i = 0; i < number; i++) {
+        final += string;
+    }
+    return final;
 };
 
 // Do not edit below this line
